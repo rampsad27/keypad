82387d23d444dcdf7db30729133c39e6f3173202
rampsad27 <sainamaskey27@gmail.com>
2023-01-11 07:02:11 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const divItem1 = document.querySelector(".prime1");
+const divItem2= document.querySelector(".prime2");
+const divItem3 = document.querySelector(".prime3");
+
+divItem1.addEventListener("click", ()=>{
+
+});
+divItem2.addEventListener("click", ()=>{
+
+});
+divItem3.addEventListener("click", ()=>{
+
+});
